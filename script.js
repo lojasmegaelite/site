34d677d923aceab7a2b6d6f34868bf0cfdd0f28f
pellemedicalcenter <dreamstoryag@gmail.com>
2024-10-03 22:36:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("whatsapp-btn").addEventListener("click", function() {
-    window.open("https://wa.me/5517981741331", "_blank");
+    window.open("https://wa.me/5517981741333", "_blank");
 });
